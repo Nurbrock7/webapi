0d53e2ff479cb7c2ac647117ce97c715fd622eea
Nur Brock <NurBrock7@gmail.com>
2022-10-05 14:56:31 +0200

    First Changes

---
+++ tracking/Data/IssueDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tracking.Models;
+
+namespace tracking.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> Options):base(Options)
+        {
+
+        }
+        public DbSet<Issue> Issues { get; set; }    
+    }
+}
